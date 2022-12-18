095f72541c6e06b7d95db6e1b6fb72c03d04212a
Ateet Agrawal <75253707+ateet19971997@users.noreply.github.com>
2022-12-18 11:04:34 +0530

    Update streamlit_app.py
    tips

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,5 +25,5 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests as rq
 fruityvice_response = rq.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response
+streamlit.text(fruityvice_response)
 
