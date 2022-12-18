fd80ec1042cc6860a82d07c9022632b4cc61e495
Ateet Agrawal <75253707+ateet19971997@users.noreply.github.com>
2022-12-18 11:06:35 +0530

    Update streamlit_app.py
    added list

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,8 @@ import requests as rq
 fruityvice_response = rq.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
