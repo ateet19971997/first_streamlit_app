c440952da2313487d6216e48946f8704806ab5b1
Ateet Agrawal <75253707+ateet19971997@users.noreply.github.com>
2022-12-19 15:31:35 +0530

    Update streamlit_app.py
    adedssdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = rq.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-  return fruityvice_nor
+  return fruityvice_normalized
 
 #New sectionmalized
 streamlit.header("Fruityvice Fruit Advice!")
